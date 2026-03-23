87b492baf34f6b772d3f565a26f11a74948f50d0
Nicholas Meyers <nicholas.meyers@hotmail.com>
2026-03-23 23:27:23 +0100

    chore: refactor CertController

--- src/main/java/be/nicholasmeyers/guardiangateway/certbot/CertController.java
+++ src/main/java/be/nicholasmeyers/guardiangateway/certbot/CertController.java
@@ -2,9 +2,11 @@ package be.nicholasmeyers.guardiangateway.certbot;
 
 import be.nicholasmeyers.guardiangateway.config.ApplicationConfig;
 import be.nicholasmeyers.guardiangateway.config.ApplicationProperties;
+import io.netty.resolver.NoopAddressResolverGroup;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import reactor.netty.http.client.HttpClient;
 
 import java.util.Map;
 import java.util.Optional;
@@ -29,7 +32,11 @@ public class CertController {
     public CertController(ChallengeStore challengeStore, ApplicationProperties applicationProperties, WebClient.Builder webClientBuilder) {
         this.challengeStore = challengeStore;
         this.applicationProperties = applicationProperties;
-        this.webClient = webClientBuilder.build();
+        this.webClient = webClientBuilder
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create().resolver(NoopAddressResolverGroup.INSTANCE)
+                ))
+                .build();
     }
 
     @GetMapping("/{token}")
